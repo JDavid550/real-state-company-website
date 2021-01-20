f3434e5a7cf3371b3a970d06835ece55acce2622
Juan David Álvarez Sarmiento <75177549+JDavid550@users.noreply.github.com>
2021-01-19 20:59:17 -0500

    contador-añadido

--- gallery.js
+++ gallery.js
@@ -1,10 +1,14 @@
 var slides=document.querySelector(".slider-items").children;
 var controls=document.querySelector('.section_4').children;
-var nextSlide=document.querySelector(".previous_button");
-var prevSlide=document.querySelector(".next_button");
+var counter=document.querySelector('.numbers-counter').children;
+var nextSlide=document.querySelector(".next_button");
+var prevSlide=document.querySelector(".previous_button");
 var totalSlides=slides.length;
+var totalCounts=counter.length;
 var index=0;
-console.log(slides);
+
+console.log(totalCounts)
+
 nextSlide.onclick=function () {
     next("next");
 }
@@ -16,7 +20,7 @@ function next(direction){
 
   if(direction=="next"){
      index++;
-      if(index==totalSlides){
+      if(index==totalSlides && index==totalCounts){
        index=0;
       }
   } 
@@ -31,7 +35,9 @@ function next(direction){
 
  for(i=0;i<slides.length;i++){
         slides[i].classList.remove("active");
+        counter[i].classList.remove('active');
  }
- slides[index].classList.add("active");     
+ slides[index].classList.add("active"); 
+ counter[index].classList.add('active');    
 
 }
